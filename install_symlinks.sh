6388af7386a225c21155efc3a5ab43c41c3f6e9a
Abdulmelik Kalkan <49363216+AbdulmelikKalkan@users.noreply.github.com>
2021-06-13 20:46:19 +0300

    Create install_symlinks.sh

---
+++ install_symlinks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#You should install vim and zsh for using these configs
+
+set -xeou pipefail
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+for f in .zshrc \
+	.vimrc \
+	.gitconfig \
+	.gitignore_global \
+	.kubectl_aliases; do
+	if [ -e "$HOME/$f" ]; then rm "$HOME/$f"; fi
+	ln -sf "$SCRIPT_DIR/$f" "$HOME/$f"
+done
+
+for f in zsh_aliases.inc \
+  zsh_functions.inc; do
+  if [ -e "$HOME/.oh-my-zsh/$f" ]; then rm "$HOME/.oh-my-zsh/$f"; fi
+  ln -sf "$SCRIPT_DIR/$f" "$HOME/.oh-my-zsh/$f"
+done
+
+# install vundle for vim plugins
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
